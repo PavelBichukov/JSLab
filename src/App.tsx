8343af85ce146d79f79707b8a33ed1d212c80ccc
Nikita <nickpelmog2003@gmail.com>
2023-09-05 15:52:36 +0300

    INT-21/FEAT: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,15 @@
-import { useEffect, useState } from 'react'
-import './App.scss'
-import axios from 'axios'
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-import { Typography } from 'components/share/Typography'
-
-const queryClient = new QueryClient()
+import { Route, Routes } from 'react-router-dom'
+import Landing from 'src/pages/Landing/Landing'
+import SignUp from 'src/pages/Sign Up/SignUp'
+import LogIn from 'src/pages/Log In/LogIn'
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Example />
-    </QueryClientProvider>
-  )
-  // const [backendData, setBackendData] = useState([] as string[])
-  //
-  // useEffect(() => {
-  //     async function doGetRequest() {
-  //         const res = await axios.get('http://localhost:5000/api')
-  //         const data: string[] = res.data.users
-  //         setBackendData(data)
-  //     }
-  //     doGetRequest()
-  // }, [])
-  //
-  // return (
-  //     <>
-  //         <h1>Get started</h1>
-  //         {typeof backendData === 'undefined' ? (
-  //             <p>Loading...</p>
-  //         ) : (
-  //             backendData.map((user, i) => <p key={i}>{user}</p>)
-  //         )}
-  //     </>
-  // )
-}
-
-function Example() {
-  const { isLoading, error, data } = useQuery('repoData', () =>
-    fetch('http://localhost:5000/api').then((res) => res.json())
-  )
-
-  if (isLoading) return 'Loading...'
-
-  if (error) return 'An error has occurred: ' + error.message
-
-  return (
-    <div>
-      <h1>Get started</h1>
-      {typeof data === 'undefined' ? (
-        <p>Loading...</p>
-      ) : (
-        data.users.map((user, i) => <p key={i}>{user}</p>)
-      )}
-    </div>
+    <Routes>
+      <Route index element={<Landing />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<LogIn />} />
+    </Routes>
   )
 }
 export default App
---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,13 @@
+import styles from './News.module.css'
+import Header from 'src/layout/Header/Header'
+
+const Landing = () => {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+    </div>
+  )
+}
+export default Landing
