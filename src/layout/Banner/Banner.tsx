2a737c5725e2138fefe5278e738a9f7b36fe6c95
Nikita <nickpelmog2003@gmail.com>
2023-09-06 14:21:50 +0300

    INT-25/FEAT: add banner

---
+++ src/layout/Banner/Banner.tsx
@@ -0,0 +1,21 @@
+import styles from './Banner.module.scss'
+
+const Banner = () => {
+  return (
+    <div className={styles.mainBanner}>
+      <div>
+        <p className={styles.pattern}>
+          Providing Businesses and Individuals an Smart way to Save, Pay &
+          Invest in Energy.
+        </p>
+      </div>
+      <div>
+        <p className={styles.pattern2}>
+          Providing Businesses and Individuals an Smart way to Save, Pay &
+          Invest in Energy.
+        </p>
+      </div>
+    </div>
+  )
+}
+export default Banner
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,10 +1,18 @@
 import Header from 'src/layout/Header/Header'
+import Banner from 'src/layout/Banner/Banner'
 
 const Landing = () => {
   return (
     <div>
       <div>
         <Header />
+      </div>
+
+      <div
+        style={{ width: '100%', height: '1000px', backgroundColor: 'yellow' }}
+      ></div>
+      <div style={{}}>
+        <Banner />
       </div>
     </div>
   )
