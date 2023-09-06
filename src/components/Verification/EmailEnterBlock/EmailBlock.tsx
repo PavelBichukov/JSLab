20477b26cb26fb36a40d99e205c97b4f802706ed
Pavel Bichukov <pbichukov@gmail.com>
2023-09-06 12:19:11 +0300

    INT-15/FIX: change components naming

--- src/components/Verification/EmailEnterBlock/EmailBlock.tsx
+++ src/components/Verification/EmailEnterBlock/EmailBlock.tsx
@@ -1,34 +1,14 @@
-import { Typography } from 'src/components/share/Typography'
+// import { Typography } from 'src/components/share/Typography'
 import styles from './EmailEnterBlock.module.scss'
 
 
-const benefitsList: string[] = [
-    'Grow your customer base',
-    'Save on transactional fees',
-    'Run custom promotions',
-    'Get access to the latest tech',
-    'Win your market',
-]
 
 export const EmailBlock = () => {
-    
+
     return(
         <div className={styles.container}>
             <div className={styles.emailBlock}>
-                <section className={styles.sideSection}>
-                    <button className={styles.backButton}>Back</button>
-                    <Typography variant='HeaderM'>Stations</Typography>
-                    <Typography variant='ParagraphL' className={styles.sideSectionDescription}> For organizations that want to grow their revenue by reducing credit card fees,
-                    while increasing traffic to their location.</Typography>
-                    <ul className={styles.benefitsList}>
-                        {
-                            benefitsList.map((item, index) => <Typography variant='ParagraphL'className={styles.listitem} key={index}>{item}</Typography>  )
-                        }
-                    </ul>
-                </section>
-                <div className={styles.formBlock}>
-                    
-                </div>
+
             </div>
         </div>
     )
--- src/components/Verification/VerificationMainComponent/Verification.tsx
+++ src/components/Verification/VerificationMainComponent/Verification.tsx
@@ -1,12 +1,37 @@
+import { Typography } from 'src/components/share/Typography'
+import styles from './Verification.module.scss'
 
 
+const benefitsList: string[] = [
+    'Grow your customer base',
+    'Save on transactional fees',
+    'Run custom promotions',
+    'Get access to the latest tech',
+    'Win your market',
+]
 
-export const Verification = () => {
+export const VerificationMainComponent = () => {
+
     return(
-        <div>
-
+        <div className={styles.container}>
+            <div className={styles.verificationBlock}>
+                <section className={styles.sideSection}>
+                    <button className={styles.backButton}>Back</button>
+                    <Typography variant='HeaderM'>Stations</Typography>
+                    <Typography variant='ParagraphL' className={styles.sideSectionDescription}> For organizations that want to grow their revenue by reducing credit card fees,
+                    while increasing traffic to their location.</Typography>
+                    <ul className={styles.benefitsList}>
+                        {
+                            benefitsList.map((item, index) => <Typography variant='ParagraphL'className={styles.listitem} key={index}>{item}</Typography>  )
+                        }
+                    </ul>
+                </section>
+                <div className={styles.formBlock}>
+                    
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Verification
+export default VerificationMainComponent
