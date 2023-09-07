c1ffefd93b62b03737a68f6ef2077e875ad4754f
Nikita <nickpelmog2003@gmail.com>
2023-09-07 15:11:31 +0300

    INT-26/FEAT: add infos for card

--- src/components/share/TeamCard/TeamCard.tsx
+++ src/components/share/TeamCard/TeamCard.tsx
@@ -1,16 +1,14 @@
 import styles from './TeamCard.module.scss'
 import { MdEmail } from 'react-icons/md'
-import Img1 from './assets/mikita.png'
 
 const TeamCard = (props) => {
-  const { src, name, jobTitle, email } = props
+  const { imgSrc, name, jobTitle, email } = props
   return (
-    <div style={{ width: '100%' }}>
-      <img src={Img1} width="300" height="300" alt={name} />
-      {/*<img src={src} alt={name} width="100px" height="100px" />*/}
-      <p>{name}</p>
-      <p>{jobTitle}</p>
-      <div>
+    <div className={styles.mainCard}>
+      <img src={imgSrc} alt={name} className={styles.imgCard} />
+      <p className={styles.cardName}>{name}</p>
+      <p className={styles.cardJobTitle}>{jobTitle}</p>
+      <div className={styles.cardEmail}>
         <MdEmail />
         {email}
       </div>
--- src/layout/Team/Team.tsx
+++ src/layout/Team/Team.tsx
@@ -1,16 +1,45 @@
 import styles from './Team.module.scss'
 import TeamCard from 'components/share/TeamCard/TeamCard'
+import imgNikita from '../../assets/images/mikita.png'
+import imgPavel from '../../assets/images/pavel.png'
+import imgAnton from '../../assets/images/anton.png'
+import imgDenis from '../../assets/images/denis.png'
+import imgDasha from '../../assets/images/dasha.png'
 
 const Team = () => {
   return (
     <div>
       <p className={styles.teamTitle}>Our development team</p>
-      <div>
+      <div className={styles.teamCards}>
         <TeamCard
-          src="../../../assets/images/mikita.png"
-          name="smt"
-          jobTitle="smt"
-          email="smt"
+          imgSrc={imgDasha}
+          name="Dasha Kavalenka"
+          jobTitle="Group Manager"
+          email="darya.kavalenka@ventionteams.com"
+        />
+        <TeamCard
+          imgSrc={imgDenis}
+          name="Denis Kozyrev"
+          jobTitle="Tech Lead"
+          email="deniskozyrev55@gmail.com"
+        />
+        <TeamCard
+          imgSrc={imgAnton}
+          name="Anton Gavrilenko"
+          jobTitle="Tech Lead"
+          email="gavrilenko7732@gmail.com"
+        />
+        <TeamCard
+          imgSrc={imgNikita}
+          name="Nikita Pelikh"
+          jobTitle="Software Developer"
+          email="mikitapelikh@gmail.com"
+        />
+        <TeamCard
+          imgSrc={imgPavel}
+          name="Pavel Bichukou"
+          jobTitle="Software Developer"
+          email="pavel.bichukou@itechart-group.com"
         />
       </div>
     </div>
