e8394b304ae9555adcd96286a08b7f25c280a04e
Pavel Bichukov <pbichukov@gmail.com>
2023-09-13 14:50:33 +0300

    INT-31/FIX: removed commented code

--- src/components/SignUp/SignUpMainComponent/SignUp.tsx
+++ src/components/SignUp/SignUpMainComponent/SignUp.tsx
@@ -32,41 +32,4 @@ export const SignUpMainComponent = () => {
   )
 }
 
-// export const SignUpMainComponent = () => {
-//   const currenStep = 2
-
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.signUpBlock}>
-//         <section className={styles.sideSection}>
-//           <div className={styles.progressBar}>
-//             {progressBarConstants.map((item) => (
-//               <div
-//                 className={
-//                   currenStep >= item.id
-//                     ? styles.progressBarItemActive
-//                     : styles.progressBarItem
-//                 }
-//                 key={item.id}
-//               >
-//                 <div
-//                   className={
-//                     currenStep >= item.id
-//                       ? styles.circleNumberActive
-//                       : styles.circleNumber
-//                   }
-//                 >
-//                   <Typography variant="LabelL">{item.id}</Typography>
-//                 </div>
-//                 <Typography variant="ParagraphL">{item.name}</Typography>
-//               </div>
-//             ))}
-//           </div>
-//         </section>
-//         <div className={styles.formBlock}></div>
-//       </div>
-//     </div>
-//   )
-// }
-
 export default SignUpMainComponent
