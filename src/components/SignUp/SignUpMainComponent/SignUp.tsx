f8373f20419df81be31e3758593a3e897ce064fe
Pavel Bichukov <pbichukov@gmail.com>
2023-09-13 14:29:53 +0300

    INT-31/FIX: used cn for combine classnames, and remove removed duplicate lines of code from styles

--- src/components/SignUp/SignUpMainComponent/SignUp.tsx
+++ src/components/SignUp/SignUpMainComponent/SignUp.tsx
@@ -1,10 +1,11 @@
+import cn from 'classnames'
 import { Typography } from 'src/components/share/Typography'
 
 import { progressBarConstants } from './progressBarConstants'
 import styles from './SignUpMainComponent.module.scss'
 
 export const SignUpMainComponent = () => {
-  const currenStep = 2
+  const currentStep = 2
 
   return (
     <div className={styles.container}>
@@ -13,20 +14,11 @@ export const SignUpMainComponent = () => {
           <div className={styles.progressBar}>
             {progressBarConstants.map((item) => (
               <div
-                className={
-                  currenStep >= item.id
-                    ? styles.progressBarItemActive
-                    : styles.progressBarItem
-                }
-                key={item.id}
+                className={cn(styles.progressBarItem, {
+                  [styles.progressBarItemActive]: currentStep >= item.id,
+                })}
               >
-                <div
-                  className={
-                    currenStep >= item.id
-                      ? styles.circleNumberActive
-                      : styles.circleNumber
-                  }
-                >
+                <div className={styles.circleNumber}>
                   <Typography variant="LabelL">{item.id}</Typography>
                 </div>
                 <Typography variant="ParagraphL">{item.name}</Typography>
@@ -39,5 +31,42 @@ export const SignUpMainComponent = () => {
     </div>
   )
 }
+
+// export const SignUpMainComponent = () => {
+//   const currenStep = 2
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.signUpBlock}>
+//         <section className={styles.sideSection}>
+//           <div className={styles.progressBar}>
+//             {progressBarConstants.map((item) => (
+//               <div
+//                 className={
+//                   currenStep >= item.id
+//                     ? styles.progressBarItemActive
+//                     : styles.progressBarItem
+//                 }
+//                 key={item.id}
+//               >
+//                 <div
+//                   className={
+//                     currenStep >= item.id
+//                       ? styles.circleNumberActive
+//                       : styles.circleNumber
+//                   }
+//                 >
+//                   <Typography variant="LabelL">{item.id}</Typography>
+//                 </div>
+//                 <Typography variant="ParagraphL">{item.name}</Typography>
+//               </div>
+//             ))}
+//           </div>
+//         </section>
+//         <div className={styles.formBlock}></div>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default SignUpMainComponent
