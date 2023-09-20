ccf99bae0ebdfa11ae2739c0748248ac0b28a0df
Pavel Bichukov <pbichukov@gmail.com>
2023-09-20 12:58:20 +0300

    INT-42/FIX: removed commented code

--- src/components/share/Input/Input.tsx
+++ src/components/share/Input/Input.tsx
@@ -7,48 +7,6 @@ import styles from './Input.module.scss'
 import { useState } from 'react'
 import { Typography } from 'components/share/Typography'
 
-// export const Input = ({
-//   variant,
-//   className,
-//   onChange,
-//   label,
-//   labelClassName,
-//   id,
-//   ...restProps
-// }: IInputProps) => {
-//   const [showPass, setShowPass] = useState(false)
-
-//   return (
-//     <div className={styles.inputBox}>
-//       <input
-//         {...restProps}
-//         className={cn(styles.input, className)}
-//         type={variant === 'password' && showPass ? 'text' : variant}
-//         placeholder=""
-//         id={id}
-//         onChange={onChange}
-//       />
-//       <label
-//         className={cn(styles.label, labelClassName)}
-//         htmlFor={id}
-//       >
-//         {label}
-//       </label>
-//       {!showPass ? (
-//         <EyeIcon
-//           className={cn(styles.eyeIcon, {[styles.eyeIconHidden] : variant !== 'password'})}
-//           onClick={() => setShowPass(!showPass)}
-//         />
-//       ) : (
-//         <ClosedEyeIcon
-//           className={cn(styles.eyeIcon, {[styles.eyeIconHidden] : variant !== 'password'})}
-//           onClick={() => setShowPass(!showPass)}
-//         />
-//       )}
-//     </div>
-//   )
-// }
-
 export const Input = ({
   variant,
   className,
