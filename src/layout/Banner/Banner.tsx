87c94596ecf9f8d8f52796a9482b2cdb5c3d8979
Nikita <nickpelmog2003@gmail.com>
2023-09-21 01:11:31 +0300

    INT-38/FEAT: add responsive for header

--- src/layout/Banner/Banner.tsx
+++ src/layout/Banner/Banner.tsx
@@ -3,7 +3,7 @@ import styles from './Banner.module.scss'
 const Banner = () => (
   <div className={styles.mainBanner}>
     <div className={styles.pattern}></div>
-    <div>
+    <div className={styles.mainText}>
       <p className={styles.patternText}>
         Providing Businesses and Individuals an
         <span className={styles.smartText}> Smart</span> way to
@@ -13,9 +13,11 @@ const Banner = () => (
         <span className={styles.smartText}> Energy</span>.
       </p>
     </div>
-    <a href="#rates">
-      <button className={styles.bannerButton}>Get started today</button>
-    </a>
+    <div className={styles.buttonContainer}>
+      <a href="#rates">
+        <button className={styles.bannerButton}>Get started today</button>
+      </a>
+    </div>
   </div>
 )
 
