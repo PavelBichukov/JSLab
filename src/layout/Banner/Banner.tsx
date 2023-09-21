265304c0d42d411b19d4a005318e8679f87622b1
Nikita <nickpelmog2003@gmail.com>
2023-09-21 22:50:04 +0300

    INT-39/FEAT: redesign styles for component

--- src/layout/Banner/Banner.tsx
+++ src/layout/Banner/Banner.tsx
@@ -1,3 +1,5 @@
+import { Typography } from 'components/share/Typography'
+
 import styles from './Banner.module.scss'
 
 const Banner = () => {
@@ -5,14 +7,14 @@ const Banner = () => {
     <div className={styles.mainBanner}>
       <div className={styles.pattern}></div>
       <div className={styles.mainText}>
-        <p className={styles.patternText}>
+        <Typography variant="Header3XL" className={styles.patternText}>
           Providing Businesses and Individuals an
           <span className={styles.smartText}> Smart</span> way to
           <span className={styles.smartText}> Save</span>,
           <span className={styles.smartText}> Pay</span> &
           <span className={styles.smartText}> Invest</span> in
           <span className={styles.smartText}> Energy</span>.
-        </p>
+        </Typography>
       </div>
       <div className={styles.buttonContainer}>
         <a href="#rates">
