63b30571d79a9f0c615f3c4342fc4ce4134680b5
Nikita <nickpelmog2003@gmail.com>
2023-11-03 14:30:03 +0300

    INT-56/FEAT: add styles for page

---
+++ src/components/AddStation/StationAmenitiesBlock/StationAmenities.constants.ts
@@ -0,0 +1,18 @@
+export const subStoreAmenities = [
+  { value: 'Groceries', label: 'Groceries' },
+  { value: 'Ice', label: 'Ice' },
+  { value: 'Alcohol', label: 'Alcohol' },
+  { value: 'Tobacco', label: 'Tobacco' },
+  { value: 'Automotive', label: 'Automotive' },
+  { value: 'Lottery', label: 'Lottery' },
+]
+
+export const stationAmenities = [
+  { value: 'Car Wash', label: 'Car Wash' },
+  { value: 'Restrooms', label: 'Restrooms' },
+  { value: 'ATM', label: 'ATM' },
+  { value: 'Food Offerings', label: 'Food Offerings' },
+  { value: 'Restaurant', label: 'Restaurant' },
+  { value: 'Overnight Parking', label: 'Overnight Parking' },
+  { value: 'Showers', label: 'Showers' },
+]
--- src/components/AddStation/StationAmenitiesBlock/StationAmenities.tsx
+++ src/components/AddStation/StationAmenitiesBlock/StationAmenities.tsx
@@ -1,7 +1,124 @@
+import { useState } from 'react'
+
+import {
+  stationAmenities,
+  subStoreAmenities,
+} from 'components/AddStation/StationAmenitiesBlock/StationAmenities.constants'
+import { Button, Checkbox, Typography } from 'components/share'
+import { ADD_STATION_STEPS } from 'src/constants/addStationSteps'
+import { setCurrentStep } from 'src/store/signUp'
+import { useAppDispatch } from 'src/utils/redux-hooks/hooks'
+
 import styles from './StationAmenities.module.scss'
 
 export const StationAmenities = () => {
-  return <div>Station Amenties</div>
+  const [perks, setPerks] = useState([])
+  const [check, setCheck] = useState(false)
+  const dispatch = useAppDispatch()
+
+  const handleCbClick = (ev) => {
+    const { checked, name } = ev.target
+    if (checked) {
+      setPerks([...perks, name])
+    } else {
+      setPerks([...perks.filter((selectedName) => selectedName != name)])
+    }
+  }
+
+  const onBack = () => {
+    dispatch(setCurrentStep(ADD_STATION_STEPS.GENERAL_INFORMATION))
+  }
+
+  const onNext = () => {
+    console.log(
+      'Полиция повязала группу мошенников, продающих дипломы в метро.\n' +
+        '«Нам пришлось их отпустить» - заявил профессор экономических наук сержант Иванов.'
+    )
+    dispatch(setCurrentStep(ADD_STATION_STEPS.CONNECT_YOUR_BANK))
+  }
+
+  return (
+    <div>
+      <form onSubmit={onNext}>
+        <div className={styles.mainContainer}>
+          <Typography variant="HeaderS">Station Amenities</Typography>
+          <div className={styles.labelBlock}>
+            <Checkbox
+              ref={null}
+              id="termsCheckBox"
+              name="Convenient Store"
+              onClick={() => setCheck(!check)}
+              onChange={handleCbClick}
+            />
+            <Typography variant="ParagraphL" className={styles.paragraphLabel}>
+              Convenient Store
+            </Typography>
+          </div>
+          <div className={styles.subDiv}>
+            {subStoreAmenities.map((subStoreAmenity) => (
+              <div key={subStoreAmenity.value} className={styles.subLabelBlock}>
+                <Checkbox
+                  ref={null}
+                  id="termsCheckBox"
+                  name={subStoreAmenity.value}
+                  onChange={handleCbClick}
+                  disabled={!check}
+                />
+                <Typography
+                  variant="ParagraphL"
+                  className={
+                    check
+                      ? styles.paragraphLabel
+                      : styles.paragraphLabelDisabled
+                  }
+                >
+                  {subStoreAmenity.label}
+                </Typography>
+              </div>
+            ))}
+          </div>
+          {stationAmenities.map((stationAmenity) => (
+            <div key={stationAmenity.value} className={styles.labelBlock}>
+              <Checkbox
+                ref={null}
+                id="termsCheckBox"
+                name={stationAmenity.value}
+                onChange={handleCbClick}
+              />
+              <Typography
+                variant="ParagraphL"
+                className={styles.paragraphLabel}
+              >
+                {stationAmenity.label}
+              </Typography>
+            </div>
+          ))}
+        </div>
+        <div className={styles.buttonsBlock}>
+          <Button
+            className={styles.buttonBack}
+            type="button"
+            mode="outlinedWhite"
+            variant="primary"
+            size="small"
+            onClick={onBack}
+          >
+            Back
+          </Button>
+          <Button
+            className={styles.buttonFinished}
+            type="submit"
+            mode={perks.length > 0 ? 'defaultBlack' : 'disabled'}
+            variant="primary"
+            size="small"
+            onClick={() => console.log('clicked')}
+          >
+            Next
+          </Button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default StationAmenities
