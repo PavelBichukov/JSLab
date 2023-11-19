80930c7af45fd367c407910fe94fc056b453b73b
Nikita <nickpelmog2003@gmail.com>
2023-11-20 00:25:58 +0300

    INT-63/FEAT: add table layout

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,73 +1,146 @@
 import {
-  MRT_Table, //import alternative sub-component if we do not want toolbars
-  type MRT_ColumnDef,
-  useMaterialReactTable,
-} from 'material-react-table'
-import { useMemo } from 'react'
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+import * as React from 'react'
 
-import { type Person, data } from './makeData'
+import { Typography } from 'components/share'
 
-export const Example = () => {
-  const columns = useMemo<MRT_ColumnDef<Person>[]>(
-    //column definitions...
-    () => [
-      {
-        accessorKey: 'firstName',
-        header: 'First Name',
-      },
-      {
-        accessorKey: 'lastName',
-        header: 'Last Name',
-      },
-      {
-        accessorKey: 'address',
-        header: 'Address',
-      },
-      {
-        accessorKey: 'city',
-        header: 'City',
-      },
-      {
-        accessorKey: 'state',
-        header: 'State',
-      },
-    ],
-    []
-    //end
-  )
+import styles from './Table.module.scss'
 
-  const table = useMaterialReactTable({
+type Person = {
+  stationName: string
+  address: string
+  latitude: string
+  longitude: string
+  lastUpdated: string
+  online: string
+}
+
+const defaultData: Person[] = [
+  {
+    stationName: 'tanner',
+    address: '55 Kibby Ln, Cincinnati, OH 45233',
+    latitude: '32.802774',
+    longitude: '-96.800143',
+    lastUpdated: 'Feb 14, 2023 • 12:00:01 AM EST',
+    online: 'str',
+  },
+  {
+    stationName: 'tandy',
+    address: '55 Kibby Ln, Cincinnati, OH 45233',
+    latitude: '32.802774',
+    longitude: '-96.800143',
+    lastUpdated: 'Feb 14, 2023 • 12:00:01 AM EST',
+    online: 'str',
+  },
+  {
+    stationName: 'joe',
+    address: '55 Kibby Ln, Cincinnati, OH 45233',
+    latitude: '32.802774',
+    longitude: '-96.800143',
+    lastUpdated: 'Feb 14, 2023 • 12:00:01 AM EST',
+    online: 'str',
+  },
+]
+
+const columnHelper = createColumnHelper<Person>()
+
+const columns = [
+  columnHelper.accessor('stationName', {
+    header: () => 'Station Name | IDs',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('address', {
+    header: () => 'Address',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('latitude', {
+    header: () => 'Latitude',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('longitude', {
+    header: () => 'Longitude',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('lastUpdated', {
+    header: () => 'Last Updated',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('online', {
+    header: () => 'Online',
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+]
+const Example = () => {
+  const [data, setData] = React.useState(() => [...defaultData])
+
+  const table = useReactTable({
+    data,
     columns,
-    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
-    enableColumnActions: false,
-    enableColumnFilters: false,
-    enablePagination: false,
-    enableSorting: false,
-    mrtTheme: (theme) => ({
-      baseBackgroundColor: theme.palette.background.default, //change default background color
-    }),
-    muiTableBodyRowProps: { hover: false },
-    muiTableProps: {
-      sx: {
-        border: '1px solid rgba(81, 81, 81, .5)',
-      },
-    },
-    muiTableHeadCellProps: {
-      sx: {
-        border: '1px solid rgba(81, 81, 81, .5)',
-        fontStyle: 'italic',
-        fontWeight: 'normal',
-      },
-    },
-    muiTableBodyCellProps: {
-      sx: {
-        border: '1px solid rgba(81, 81, 81, .5)',
-      },
-    },
+    getCoreRowModel: getCoreRowModel(),
   })
 
-  //using MRT_Table instead of MaterialReactTable if we do not need any of the toolbar components or features
-  return <MRT_Table table={table} />
+  return (
+    <div className={styles.table}>
+      <table className={styles.tableContainer}>
+        <thead className={styles.header}>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className={styles.headerTr}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} className={styles.headerTh}>
+                  <Typography variant="LabelS" className={styles.headerLabel}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className={styles.tbody}>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className={styles.trStyle}>
+              <td>{row.original.stationName}</td>
+              <td className={styles.addressTd}>
+                <Typography variant="ParagraphL">
+                  {row.original.address}
+                </Typography>
+              </td>
+              <td className={styles.latAndLogTd}>
+                <Typography variant="ParagraphL">
+                  {row.original.latitude}
+                </Typography>
+              </td>
+              <td className={styles.latAndLogTd}>
+                <Typography variant="ParagraphL">
+                  {row.original.longitude}
+                </Typography>
+              </td>
+              <td className={styles.addressTd}>
+                <Typography variant="ParagraphL">
+                  {row.original.lastUpdated}
+                </Typography>
+              </td>
+              <td>{row.original.online}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Example
