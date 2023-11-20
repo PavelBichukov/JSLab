3d297ae71f0051a735acdd4c902cb5fb32d779fb
Nikita <nickpelmog2003@gmail.com>
2023-11-20 17:45:54 +0300

    INT-57/FIX: quick fix

--- src/components/share/MainLayout/MainLayout.tsx
+++ src/components/share/MainLayout/MainLayout.tsx
@@ -29,7 +29,7 @@ const MainLayout = ({
         </div>
         {headerActions && <div>{headerActions}</div>}
       </div>
-      <div className={cn(mainClassName)}>{children}</div>
+      <div className={mainClassName}>{children}</div>
     </div>
   </div>
 )
