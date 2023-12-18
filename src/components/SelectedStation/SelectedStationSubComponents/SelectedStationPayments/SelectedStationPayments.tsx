331ae4c24640b0f241d0de97a4cab4b4da2fcdf8
Pavel Bichukov <pbichukov@gmail.com>
2023-12-18 13:23:58 +0300

    INT-77/FEAT: added plug in payments tab

--- src/components/SelectedStation/SelectedStationSubComponents/SelectedStationPayments/SelectedStationPayments.tsx
+++ src/components/SelectedStation/SelectedStationSubComponents/SelectedStationPayments/SelectedStationPayments.tsx
@@ -1,5 +1,15 @@
+import { Checkbox, Select, Typography } from 'components/share'
+
+import styles from './SelectedStationPayments.module.scss'
+
 const SelectedStationPayments = () => {
-  return <div>Payments</div>
+  return (
+    <div className={styles.main}>
+      <Typography className={styles.tittle} variant="HeaderL">
+        Coming soon...
+      </Typography>
+    </div>
+  )
 }
 
 export default SelectedStationPayments
